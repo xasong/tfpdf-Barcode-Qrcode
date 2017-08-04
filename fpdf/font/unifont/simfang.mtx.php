e8724b40842d591546a189e00cc36ad35508cc77
xasong <30643708+xasong@users.noreply.github.com>
2017-08-04 14:35:53 +0800

    1.0.1
    1.0.1

---
+++ fpdf/font/unifont/simfang.mtx.php
@@ -0,0 +1,19 @@
+<?php
+$name='FangSong';
+$type='TTF';
+$desc=array (
+  'Ascent' => 859,
+  'Descent' => -141,
+  'CapHeight' => 668,
+  'Flags' => 4,
+  'FontBBox' => '[-8 -164 1004 859]',
+  'ItalicAngle' => 0,
+  'StemV' => 87,
+  'MissingWidth' => 1000,
+);
+$up=-86;
+$ut=47;
+$ttffile= dirname(__FILE__).'/simfang.ttf';
+$originalsize=10576012;
+$fontkey='dejavu';
+?>
